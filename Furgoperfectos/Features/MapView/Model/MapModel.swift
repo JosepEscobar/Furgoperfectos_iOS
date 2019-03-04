2484825c8081ba24e24f66693105adf938e2085f
Josep Escobar <josep.escobar-navarro@dxc.com>
2019-03-04 20:56:20 +0100

    add create project structure.. add icons add images add storyboard structire

---
+++ Furgoperfectos/Features/MapView/Model/MapModel.swift
@@ -0,0 +1,13 @@
+//
+//  MapModel.swift
+//  Furgoperfectos
+//
+//  Created by Josep Escobar on 04/03/2019.
+//  Copyright © 2019 Josep Escobar. All rights reserved.
+//
+
+import UIKit
+
+class MapModel: NSObject {
+
+}
---
+++ Furgoperfectos/Features/MapView/Repository/API Client/MapRepository.swift
@@ -0,0 +1,13 @@
+//
+//  MapRepository.swift
+//  Furgoperfectos
+//
+//  Created by Josep Escobar on 04/03/2019.
+//  Copyright © 2019 Josep Escobar. All rights reserved.
+//
+
+import UIKit
+
+class MapRepository: NSObject {
+
+}
